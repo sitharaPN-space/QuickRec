c0c65d1286bce3cac2f747ad333f1cc3ef38f5a9
Chirath Timodh <chirathtimodh@gmail.com>
2023-07-24 06:50:58 +0530

    ui modified

--- client/src/components/DetailCard.jsx
+++ client/src/components/DetailCard.jsx
@@ -7,54 +7,67 @@ import {
   Grid,
   useTheme,
   useMediaQuery,
+  IconButton,
 } from "@mui/material";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
-import ButtonComp from "./ButtonComp";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditNoteIcon from "@mui/icons-material/EditNote";
 
-const DetailCard = ({ detail }) => {
-  const subTitle = `Posted days ago`;
+const DetailCard = ({ detail, onDelete, onEdit }) => {
   const isMobile = useMediaQuery("(max-width: 600px)");
   const theme = useTheme();
 
   const getDate = (date) => {
     const [day, month, dd, year] = date.$d.toDateString().split(" ");
-    return month + " " + year;
+    return `${month ?? "m"} ${year ?? "y"}`;
   };
-
-  const handleClick = () => {};
 
   return (
     <Card sx={{ width: "100%", textAlign: "left", mb: "2px" }}>
       <CardHeader
+        sx={{ pb: "0" }}
         title={detail.institute}
-        // sx={{ backgroundColor: theme.palette.secondary[400], p: "12px" }}s
-
         titleTypographyProps={{
           fontSize: (isMobile) => (isMobile ? "1.2rem" : "1.5rem"),
           fontWeight: 600,
-          height: "10px",
         }}
+        action={
+          <div>
+            <IconButton
+              sx={{ color: theme.palette.secondary[900] }}
+              onClick={onDelete}
+            >
+              <DeleteIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: theme.palette.secondary[900] }}
+              onClick={onEdit}
+            >
+              <EditNoteIcon />
+            </IconButton>
+          </div>
+        }
       />
       <CardContent
         sx={{ fontWeight: 550, color: theme.palette.secondary[800] }}
       >
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={isMobile ? 12 : 4}>
+          <Grid item xs={isMobile ? 12 : 5}>
             <Typography fontSize={isMobile ? "14px" : "16px"}>
               {detail.qualification}
             </Typography>
           </Grid>
-          <Grid item xs={isMobile ? 12 : 8}>
+          <Grid item xs={isMobile ? 12 : 7}>
             <Typography fontSize={isMobile ? "14px" : "16px"}>
               Specialized in {detail.feild}
             </Typography>
           </Grid>
-          <Grid item xs={isMobile ? 12 : 8}>
-            <Typography fontSize={isMobile ? "14px" : "16px"}>
-              From {getDate(detail.startDate)} to {getDate(detail.endDate)}
-            </Typography>
-          </Grid>
+          {detail.startDate && detail.endDate && (
+            <Grid item xs={12}>
+              <Typography fontSize={isMobile ? "14px" : "16px"}>
+                From {getDate(detail.startDate)} to {getDate(detail.endDate)}
+              </Typography>
+            </Grid>
+          )}
         </Grid>
       </CardContent>
     </Card>
