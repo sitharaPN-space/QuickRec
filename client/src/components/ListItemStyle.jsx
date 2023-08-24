725be0e1f92aa7681c663bf95fbfaf01ce947726
duesacrifice69 <chirathtimodh@gmail.com>
2023-08-24 06:37:37 +0530

    comp modified

--- client/src/components/ListItemStyle.jsx
+++ client/src/components/ListItemStyle.jsx
@@ -3,10 +3,13 @@ import { ListItemButton } from "@mui/material";
 
 const ListItemStyle = styled(ListItemButton)(({ theme }) => ({
   textDecoration: "none",
+  height: "50px",
+  boxSizing: "border-box",
   color: theme.palette.secondary[900],
   fontWeight: 600,
   fontSize: "20px",
   "&:hover": {
+    transition: "0.5s all ease-in-out",
     bakcground: "none",
     borderBottom: `5px solid ${theme.palette.primary[500]}`,
   },
