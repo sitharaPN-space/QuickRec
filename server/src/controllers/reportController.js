d46be6ac11254d906a9967b18e81573b181397fb
duesacrifice69 <chirathtimodh@gmail.com>
2023-09-13 15:15:27 +0530

    script tag bug fixed

--- server/src/controllers/reportController.js
+++ server/src/controllers/reportController.js
@@ -1,22 +1,25 @@
 import puppeteer from "puppeteer";
-import { generateHTML } from "../../reports/applicantsReport.js";
+import { html } from "../../reports/applicantsReport.js";
 
 export const ApplicanatsReport = async (req, res) => {
+  const file = await html(req.body.vacancyId);
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-  await page.setContent(generateHTML(), { waitUntil: "domcontentloaded" });
+  page.isJavaScriptEnabled(true);
+  await page.setContent(file, {
+    waitUntil: "domcontentloaded",
+  });
   await page.emulateMediaType("screen");
   const pdf = await page.pdf({
     printBackground: true,
     format: "A4",
     landscape: true,
+    margin: { top: 50, bottom: 50, left: 50, right: 50 },
   });
   await browser.close();
   res.setHeader("Content-Length", pdf.length);
   res.setHeader("Content-Type", "application/pdf");
-  res.setHeader(
-    "Content-Disposition",
-    "attachment; filename=ApplicantsReport.pdf"
-  );
+  res.setHeader("Content-Disposition", "RPTScheduleDetails.pdf");
+  res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
   res.send(pdf);
 };
