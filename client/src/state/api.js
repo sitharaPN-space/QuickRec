c2217bcf7199ba26e83cbb83d39b7894c80adc84
duesacrifice69 <chirathtimodh@gmail.com>
2023-09-20 12:29:42 +0530

    application api endpoints added

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["Vacancy", "AppBasicDetails"],
+  tagTypes: ["Vacancy", "AppBasicDetails", "AppEduDetails"],
   endpoints: (builder) => ({
     getVacancyBySearch: builder.query({
       query: (searchQuery) => `/vacancy/search/?searchQuery=${searchQuery}`,
@@ -15,6 +15,12 @@ export const api = createApi({
       }),
       providesTags: ["AppBasicDetails"],
     }),
+    getAppEduDetails: builder.query({
+      query: ({ userId }) => ({
+        url: `/application/education/?userId=${userId}`,
+      }),
+      providesTags: ["AppEduDetails"],
+    }),
     createAppBasicDetails: builder.mutation({
       query: (createReq) => ({
         url: `/application/addBasicDetails`,
@@ -23,6 +29,27 @@ export const api = createApi({
       }),
       invalidatesTags: ["AppBasicDetails"],
     }),
+    createAppEducation: builder.mutation({
+      query: (createReq) => ({
+        url: `/application/addEduDetails`,
+        method: "POST",
+        body: createReq,
+      }),
+    }),
+    createAppExperience: builder.mutation({
+      query: (createReq) => ({
+        url: `/application/addExpDetails`,
+        method: "POST",
+        body: createReq,
+      }),
+    }),
+    createAppAchievement: builder.mutation({
+      query: (createReq) => ({
+        url: `/application/addOtherDetails`,
+        method: "POST",
+        body: createReq,
+      }),
+    }),
   }),
 });
 
@@ -30,5 +57,9 @@ export const {
   useGetVacanciesQuery,
   useGetVacancyBySearchQuery,
   useCreateAppBasicDetailsMutation,
+  useCreateAppEducationMutation,
+  useCreateAppExperienceMutation,
+  useCreateAppAchievementMutation,
   useGetAppBasicDetailsQuery,
+  useGetAppEduDetailsQuery,
 } = api;
