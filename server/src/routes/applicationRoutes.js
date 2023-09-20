0002083594e4958188efce80f6813dc901283e7e
duesacrifice69 <chirathtimodh@gmail.com>
2023-09-20 12:44:15 +0530

    application routes modified

--- server/src/routes/applicationRoutes.js
+++ server/src/routes/applicationRoutes.js
@@ -4,13 +4,29 @@ import {
   getApplications,
   getAppDetails,
   approveQualification,
+  reviewApplication,
+  getAppBasicDetails,
+  getAppEducation,
+  getAppExperience,
+  getAppOtherDetails,
+  addEducation,
+  addExperience,
+  addOtherDetails,
 } from "../controllers/applicationController.js";
 
 const router = express.Router();
 
 router.post("/addBasicDetails", addBasicDetails);
+router.post("/addEduDetails", addEducation);
+router.post("/addExpDetails", addExperience);
+router.post("/addOtherDetails", addOtherDetails);
 router.post("/approve", approveQualification);
+router.post("/reviewed", reviewApplication);
 router.get("/userApplication", getAppDetails);
+router.get("/basicDetails", getAppBasicDetails);
+router.get("/education", getAppEducation);
+router.get("/experience", getAppExperience);
+router.get("/otherDetails", getAppOtherDetails);
 router.get("/allApplications", getApplications);
 
 export default router;
