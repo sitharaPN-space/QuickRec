62825aabaf9bd7d2eeb45776c36c16b4b786309f
duesacrifice69 <chirathtimodh@gmail.com>
2023-09-26 13:35:29 +0530

    reportService added

--- server/src/controllers/reportController.js
+++ server/src/controllers/reportController.js
@@ -1,29 +1,14 @@
-import puppeteer from "puppeteer";
-import { html } from "../../reports/applicantsReport.js";
+import { RPTScheduleDetails } from "../services/reportService.js";
 
 export const ApplicanatsReport = async (req, res) => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    userDataDir: "./reports/tmp",
-  });
-
-  const file = html();
-  const page = await browser.newPage();
-  page.isJavaScriptEnabled(true);
-  await page.setContent(file, {
-    waitUntil: "domcontentloaded",
-  });
-  await page.emulateMediaType("screen");
-  const pdf = await page.pdf({
-    printBackground: true,
-    format: "A4",
-    landscape: true,
-    margin: { top: 50, bottom: 50, left: 50, right: 50 },
-  });
-  res.setHeader("Content-Length", pdf.length);
-  res.setHeader("Content-Type", "application/pdf");
-  res.setHeader("Content-Disposition", "RPTScheduleDetails.pdf");
-  res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
-  res.send(pdf);
-  await browser.close();
+  try {
+    const report = await RPTScheduleDetails();
+    res.setHeader("Content-Length", report.length);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "RPTScheduleDetails.pdf");
+    res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+    res.send(report);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
